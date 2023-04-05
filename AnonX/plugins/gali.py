d2fe0cf419a6c7937bbee6624e9ed24136377c0a
SANGRAM-MUSIC <120346630+SANGRAM-MUSIC@users.noreply.github.com>
2023-04-05 23:32:33 +0530

    Update gali.py

--- AnonX/plugins/gali.py
+++ AnonX/plugins/gali.py
@@ -167,10 +167,10 @@ GALI = [
     & ~BANNED_USERS
 )
 @AdminRightsCheck
-async def pause_admin(cli, message: Message, _, chat_id):
+async def is_admin(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
         return await message.reply_text(_["general_2"])
-    if not await is_music_playing(chat_id):
+    if not await admin(chat_id):
         return await message.reply_text(_["admin_1"])
     await music_off(chat_id)
     await Anon.pause_stream(chat_id)
